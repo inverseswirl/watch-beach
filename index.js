c370a6d93e2fef1fbde9e2580cfb46588c9e017d
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2020-10-29 02:51:34 +1100

    new

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-
-
-
-
 function timer(){
   var hours= new Date().getHours();
   var minutes= new Date().getMinutes();
- var seconds= new Date().getSeconds()
- am_Pm= "a.m";
+  var seconds= new Date().getSeconds()
+  am_Pm= "a.m";
+
   hours=hours<10? "0"+hours:hours;
   minutes=minutes<10? "0"+minutes:minutes;
   seconds= seconds<10? "0"+seconds:seconds;
   var time= 00+":"+ 00+":"+ 00 +" " + am_Pm;
-var time= hours+":"+ minutes+":"+ seconds +" " +am_Pm;
-var element= document.getElementById('timer');
-element.innerHTML= time;
 
-anotherElement=  document.getElementById('box1');
-secondElement=  document.getElementById('box3');
-thirdElement=  document.getElementById('box5');
-fourthElement=  document.getElementById('box7');
-fifthElement=document.getElementById('box9');
-sixthElement=document.getElementById('box11')
+  var time= hours+":"+ minutes+":"+ seconds +" " +am_Pm;
+
+ var element= document.getElementById('timer');
+ element.innerHTML= time;
+
+ anotherElement=  document.getElementById('box1');
+ secondElement=  document.getElementById('box3');
+ thirdElement=  document.getElementById('box5');
+ fourthElement=  document.getElementById('box7');
+ fifthElement=document.getElementById('box9');
+ sixthElement=document.getElementById('box11')
 
 if( seconds%2==0){
   anotherElement.style.opacity= 0.1;

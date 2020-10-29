6e091aab7121b0a4c119c9a8197b08cfa2b84492
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2020-10-29 15:37:55 +1100

    twinkle

--- index.js
+++ index.js
@@ -18,30 +18,9 @@ function timer(){
 
   var date= new Date().toDateString();
   var dateElement= document.getElementById('date');
+
   dateElement.innerHTML= date;
- anotherElement=  document.getElementById('box1');
- secondElement=  document.getElementById('box3');
- thirdElement=  document.getElementById('box5');
- fourthElement=  document.getElementById('box7');
- fifthElement=document.getElementById('box9');
- sixthElement=document.getElementById('box11')
-
-if( seconds%2==0){
-  anotherElement.style.opacity= 0.2;
-  secondElement.style.opacity= 0.2;
-  thirdElement.style.opacity= 0.2;
-  fourthElement.style.opacity= 0.2;
-  fifthElement.style.opacity=0.2;
-  sixthElement.style.opacity=0.2;
- } else{
-  anotherElement.style.opacity=0.6;
-  secondElement.style.opacity= 0.6;
-  thirdElement.style.opacity= 0.6;
-  fourthElement.style.opacity= 0.6;
-  fifthElement.style.opacity=0.6;
-  sixthElement.style.opacity=0.6;
- }
-
+ 
   
 
  
